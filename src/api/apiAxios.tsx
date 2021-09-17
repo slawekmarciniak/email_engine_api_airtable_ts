7d8cc43c6ad55007cf61deecac0004b3edea6772
Slawek <61154596+Ultran@users.noreply.github.com>
2021-09-17 13:56:28 +0200

    refactor api axios

--- src/api/apiAxios.tsx
+++ src/api/apiAxios.tsx
@@ -1,5 +1,3 @@
-// in progress
-
 import axios from "axios";
 
 // const {
@@ -9,11 +7,11 @@ import axios from "axios";
 // } = process.env;
 
 const baseUrl: string = "https://api.airtable.com/v0";
+const apiKey: string = "keyyww0OQZOBnRbMM";
 const apiConfig = {
   subscribers: `${baseUrl}/appiitjRQk15Yc6Va/Table%201`,
   campaigns: `${baseUrl}/app5OxZIkcgEJUvn0/Table%201`,
 };
-const apiKey: any = "keyyww0OQZOBnRbMM";
 const requestConfig = {
   headers: {
     Authorization: `Bearer ${apiKey}`,
@@ -32,21 +30,13 @@ export const addToAirtableDb = async (data: object, db: string) => {
   const db_url =
     db === "subscribers" ? apiConfig.subscribers : apiConfig.campaigns;
   axios
-    .post(
-      db_url,
-      { fields: { ...data } },
-      { headers: { Authorization: `Bearer ${apiKey}` } }
-    )
+    .post(db_url, { fields: { ...data } }, requestConfig)
     .then((response) => console.log(response.data))
     .catch((error) => console.log(error));
 };
 
 export const deleteCampaigne = async (id: string) => {
   axios
-    .delete(`${apiConfig.campaigns}/${id}`, {
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-      },
-    })
+    .delete(`${apiConfig.campaigns}/${id}`, requestConfig)
     .then((response) => console.log(response));
 };
